db36ab8f7927ea08ffd4b5b0a5ae64ce9f71c508
serggl <sergey.glukhov@gmail.com>
2016-11-23 15:21:50 +0300

    address rails 4 compatibility issue #2

--- lib/rails_data_migrations/migrator.rb
+++ lib/rails_data_migrations/migrator.rb
@@ -13,7 +13,8 @@ module RailsDataMigrations
 
     class << self
       def get_all_versions(connection = ActiveRecord::Base.connection)
-        if connection.data_source_exists?(schema_migrations_table_name)
+        table_check_method = connection.respond_to?(:data_source_exists?) ? :data_source_exists? : :table_exists?
+        if connection.send(table_check_method, schema_migrations_table_name)
           LogEntry.all.map { |x| x.version.to_i }.sort
         else
           []
