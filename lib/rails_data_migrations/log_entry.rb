c420f9f58f2eb49bbafa778239c19863675939cf
Michael Olson <michael.olson@notarize.com>
2022-02-22 14:29:25 -0600

    Added environment variables as configuration options for migration path, table name, and index name

--- lib/rails_data_migrations/log_entry.rb
+++ lib/rails_data_migrations/log_entry.rb
@@ -2,11 +2,11 @@ module RailsDataMigrations
   class LogEntry < ::ActiveRecord::SchemaMigration
     class << self
       def table_name
-        ActiveRecord::Base.table_name_prefix + 'data_migrations' + ActiveRecord::Base.table_name_suffix
+        ActiveRecord::Base.table_name_prefix + ENV.fetch('DATA_MIGRATIONS_TABLE_NAME', 'data_migrations') + ActiveRecord::Base.table_name_suffix
       end
 
       def index_name
-        "#{table_name_prefix}unique_data_migrations#{table_name_suffix}"
+        "#{table_name_prefix}#{ENV.fetch('DATA_MIGRATIONS_INDEX_NAME', 'unique_data_migrations')}#{table_name_suffix}"
       end
     end
   end
